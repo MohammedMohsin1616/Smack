816109b65fb3560fd5fd01965ad0b50be8f36544
Mohammed <mohammedmohsin68217@gmail.com>
2018-12-01 13:17:18 +0200

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Mohammed Mohsin Sayed on 12/1/18.
+//  Copyright © 2018 Mohammed Mohsin Sayed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
